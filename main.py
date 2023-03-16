7a5096fe40dbfd25f867238132d90ba2e1dcc190
Karsten <parceval3000@gmail.com>
2023-03-16 18:46:39 +0100

    v2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import sys
 import random
 
+
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
@@ -23,19 +24,23 @@ pigeon = pygame.transform.scale(pigeon, (50, 50))
 
 bullet = pygame.image.load("bullet.png")
 bullet = pygame.transform.scale(bullet, (20, 20))
+# Load sound files
+shoot_sound = pygame.mixer.Sound("shoot_sound.wav")
+scream_sound = pygame.mixer.Sound("scream_sound.wav")
 
 class GameObject:
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, image, speed=0):
         self.x = x
         self.y = y
         self.image = image
+        self.speed = speed
 
     def draw(self, win):
         win.blit(self.image, (self.x, self.y))
 
 def main():
     tank_obj = GameObject(WIDTH // 2 - 50, HEIGHT - 120, tank)
-    pigeons = [GameObject(random.randint(0, WIDTH - 50), random.randint(0, HEIGHT // 2), pigeon) for _ in range(10)]
+    pigeons = [GameObject(random.randint(0, WIDTH - 50), random.randint(0, HEIGHT // 2), pigeon, random.choice([-3, -2, -1, 1, 2, 3])) for _ in range(10)]
     bullets = []
 
     clock = pygame.time.Clock()
@@ -54,6 +59,8 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     bullets.append(GameObject(tank_obj.x + 40, tank_obj.y, bullet))
+                    shoot_sound.play()  # Play shooting sound
+
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and tank_obj.x > 0:
@@ -70,6 +77,7 @@ def main():
                 if b.x < p.x + 50 and b.x > p.x - 20 and b.y < p.y + 50 and b.y > p.y - 20:
                     bullets.remove(b)
                     pigeons.remove(p)
+                    scream_sound.play()  # Play scream sound
                     break
 
         tank_obj.draw(win)
@@ -78,6 +86,10 @@ def main():
         for b in bullets:
             b.draw(win)
 
+        for p in pigeons:
+            p.x += p.speed
+            if p.x < 0 or p.x > WIDTH - 50:
+                p.speed = -p.speed
         pygame.display.update()
 
 if __name__ == "__main__":
